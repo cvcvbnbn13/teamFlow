49c8c861e66274c32dd2e3f98f1fb46d1fae524a
Shau Chien <cvcvbnbn13@gmail.com>
2025-11-18 21:51:13 +0800

    theme-toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <>
+      <div>Hello World</div>
+      <ThemeToggle />
+    </>
   );
 }
+
