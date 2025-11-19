efe6634f57823db5521057679992a6d0590bcbb0
Shau Chien <cvcvbnbn13@gmail.com>
2025-11-19 21:52:02 +0800

    landing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { HeroHeader } from "./_components/header";
+import HeroSection from "./_components/hero-section";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+
+export default function Home() {
+  return (
+    <>
+      <HeroHeader />
+      <HeroSection />
+    </>
+  );
+}
